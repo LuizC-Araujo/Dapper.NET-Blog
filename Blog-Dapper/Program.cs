f24b7f2e3db76f70de5338ca066be066de9f178c
Luiz Araujo <arauldesign@gmail.com>
2022-07-19 11:50:15 -0300

    criando modelo user

--- Blog-Dapper/Program.cs
+++ Blog-Dapper/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+using Blog_Dapper.Models;
+
 namespace Blog_Dapper
 {
-    internal class Program
+    class Program
     {
+        private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True";
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            ReadUsers();
+        }
+
+        public static void ReadUsers()
+        {
+            using(var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var users = connection.GetAll<User>();
+
+                foreach(var user in users)
+                {
+                    Console.WriteLine(user.Nome);
+                }
+            }
         }
     }
 }
