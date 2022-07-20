5514336d1e88c911be158b9c3df6d3e3736aa496
Luiz Araujo <arauldesign@gmail.com>
2022-07-20 11:52:27 -0300

    repositorio generico

--- Blog-Dapper/Repositories/Repository.cs
+++ Blog-Dapper/Repositories/Repository.cs
@@ -14,5 +14,27 @@ namespace Blog_Dapper.Repositories
 
         public IEnumerable<TModel> Get()
             => _connection.GetAll<TModel>();
+
+        public TModel Get(int id)
+            => _connection.Get<TModel>(id);
+
+        public void Create(TModel model)
+            => _connection.Insert<TModel>(model);
+
+        public void Update(TModel model)
+        {
+           _connection.Update<TModel>(model);
+        }
+
+        public void Delete(TModel Model)
+        {
+           _connection.Delete<TModel>(Model);
+        }
+
+        public void Delete(int id)
+        {
+            var user = _connection.Get<TModel>(id);
+            _connection.Delete<TModel>(user);
+        }
     }
 }
