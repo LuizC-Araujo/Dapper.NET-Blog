79c4dabcd0483383cb6542e8b19dd280d7cb056e
Luiz Araujo <arauldesign@gmail.com>
2022-07-20 16:08:21 -0300

    arrumando validações no delete

--- Blog-Dapper/Screens/TagScreens/DeleteTagScreen.cs
+++ Blog-Dapper/Screens/TagScreens/DeleteTagScreen.cs
@@ -16,26 +16,39 @@ namespace Blog_Dapper.Screens.TagScreens
             Console.WriteLine("Delete Tag");
             Console.WriteLine("=================");
             Console.WriteLine("Id: ");
-            var id = Console.ReadLine();
+           
+            string option;
+            int id, count = 0;
+            do
+            {
+                if (count > 0)
+                    Console.WriteLine("Por favor, digite um número!");
+                count++;
+                option = Console.ReadLine();
+            } while (Int32.TryParse(option, out id).Equals(false));
 
-            Delete(int.Parse(id));
-
+            var retorno = Delete(id);
             Console.ReadKey();
-            MenuTagScreen.Load();
+            if (!retorno)
+                Load();
+            else
+                MenuTagScreen.Load();
         }
 
-        private static void Delete(int id)
+        private static bool Delete(int id)
         {
             try
             {
                 var repository = new Repository<Tag>(Database.Connection);
                 repository.Delete(id);
                 Console.WriteLine("Tag excluída com sucesso");
+                return true;
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Não foi possível excluir a tag");
                 Console.WriteLine($"Erro: {ex.Message}");
+                return false;
             }
         }
     }
