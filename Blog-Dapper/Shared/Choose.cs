3de31e7f3c70b45ac495d69e4773b73b4428bff8
Luiz Araujo <arauldesign@gmail.com>
2022-07-20 16:04:02 -0300

    criando a escolha do usuário

---
+++ Blog-Dapper/Shared/Choose.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog_Dapper.Shared
+{
+   public class Choose
+    {
+        public static int ChooseNumber()
+        {
+            bool verification;
+            int option, count = 0;
+            do
+            {
+                if (count > 0)
+                {
+                    Console.WriteLine("Não é um número. Digite novamente: ");
+                }
+                count++;
+                var choose = Console.ReadLine();
+                verification = Int32.TryParse(choose, out option);
+            } while (verification.Equals(false));
+
+            return option;
+        }
+    }
+}
